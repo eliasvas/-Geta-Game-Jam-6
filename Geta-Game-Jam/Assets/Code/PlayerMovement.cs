de985a381e703313114d34f17c475d2422528d04
vaselas99 <vaselas99@gmail.com>
2019-01-05 10:05:08 +0200

    added basic player movement

---
+++ Geta-Game-Jam/Assets/Code/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public int speed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        
+        //Animator.setFloat("Vertical", movement.x);
+        //Animator.setFloat("Horizontal", movement.y);
+
+        transform.position = transform.position + movement * Time.deltaTime * speed;
+	}
+}
